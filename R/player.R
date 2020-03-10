d159b3960aef4f1ce91abb1efd769562e0264cad
Ben Raymond <ben@theraymonds.org>
2020-03-11 10:20:32 +1100

    oops, fix old package name

--- R/player.R
+++ R/player.R
@@ -9,7 +9,7 @@
 #' @export
 ov_video_js <- function(youtube = FALSE) {
     assert_that(is.flag(youtube), !is.na(youtube))
-    js <- readLines(system.file("extdata/js/vid.js", package = "dvideo"))
+    js <- readLines(system.file("extdata/js/vid.js", package = "ovideo"))
     js <- paste(js, collapse = "\n")
     out <- list(tags$script(HTML(js)), if (youtube) tags$script(src = "https://www.youtube.com/iframe_api"))
     tagList(Filter(Negate(is.null), out))
